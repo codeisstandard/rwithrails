401531f6207b8982c7661792c697c359f97e20de
Michael Jordan <mjordan9@gmail.com>
2015-05-22 16:55:15 -0400

    replace image generating script with one test script

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,19 +6,28 @@ class MainController < ApplicationController
   end
 
   def r_script
-    script = params[:script]
-
-    if script == 'Chick Weights'
-      chick_weights
-    elsif script == 'Random Barplot'
-      random_barplot
-    elsif script == 'Color Styles'
-      color_styles
-    elsif script == 'Overlayed Histogram'
-      overlayed_histogram
+    r = RinRuby.new
+    n = 10
+    beta_0 = 1
+    beta_1 = 0.25
+    alpha = 0.05
+    seed = 23423
+    R.x = (1..n).entries
+    R.eval <<-EOF
+        set.seed(#{seed})
+        y <- #{beta_0} + #{beta_1}*x + rnorm(#{n})
+        fit <- lm( y ~ x )
+        est <- round(coef(fit),3)
+        pvalue <- summary(fit)$coefficients[2,4]
+    EOF
+    statement =  "E(y|x) ~= #{R.est[0]} + #{R.est[1]} * x"
+    if R.pvalue < alpha
+      response = "Reject the null hypothesis and conclude that x and y are related."
+    else
+      response = "There is insufficient evidence to conclude that x and y are related."
     end
-
-    @script = script
-
+    @results = [statement, response]
+    r.quit
+    r = RinRuby.new(false)
   end
 end
